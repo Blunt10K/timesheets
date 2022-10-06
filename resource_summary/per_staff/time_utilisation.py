b3dbd0ef065dc4aadd5699a854c32ea3f7ddf79c
Kenny <kburawudi@yahoo.com>
2022-10-06 14:32:31 +0200

    include interactivity on per staff treemap

--- resource_summary/per_staff/time_utilisation.py
+++ resource_summary/per_staff/time_utilisation.py
@@ -2,53 +2,62 @@ import streamlit as st
 import numpy as np
 from utils import *
 
-# @st.cache
-def load_data(wb):
-    # exclude nonworking records
-    df = wb.loc[wb['time']!='Nonworking']
-
-    # merge Time Category and Phase information in one -- activity
-    non_audit = df[df['Time Category'].isna()]['Assignment Name']
-    audit = df[~df['Time Category'].isna()]['Time Category']
-    df['activity'] = pd.concat((non_audit,audit))
-
-    # extract year from date, calculate working hours
-    df['Date'] = pd.to_datetime(df['Date'])
-    df = df.assign(year_month=df['Date'].dt.strftime('%B, %Y'))
-    df = df.assign(working_hours=df[['Project Hours', 'Admin Hours']].sum(axis=1))
-
-    df = df.groupby(['Resource','year_month','time','activity']).sum().drop(columns=['Project Hours', 'Admin Hours',
-        'Nonworking Hours', 'Holiday', 'Total (All Entries)',
-        'Utilization (All Entries)'])
-
-    df['monthly_hours'] = df.groupby(level = [0,1]).sum()
-    df['time_category_hours'] = df.groupby(level = [0,1,2]).sum()[['working_hours']]
-
-    df['time_category_percentage']  = (100*df['time_category_hours']/df['monthly_hours']).round(1)
-    df['activity_percentage'] = (100*df['working_hours']/df['time_category_hours']).round(1)
-
-    df = df.reset_index()
-
-    df.replace(np.nan,None,inplace=True)
-
-
-    df = df.astype({'time_category_percentage':'string','activity_percentage':'string'})
-    df['time_category_percentage']+='%'
-    df['activity_percentage']+='%'
-
-    df['time_stat'] = df['time_category_hours'].astype('string') + ' hours (' + df["time_category_percentage"] + ')'
-    df['activity_hours']  =  df['working_hours'].astype('string') + ' hours (' + df["activity_percentage"] + ')'
-
-    return df
-
-
-wb = preprocess_data()
-df = load_data(wb)
-
-fig = time_utilisation(df)
-
 
 st.set_page_config(layout='centered',page_title='Charged hours breakdwon per staff')
+
+
+
+@st.cache
+def load_data():
+    # exclude nonworking records
+    # df = wb.loc[wb['time']!='Nonworking']
+
+    # # merge Time Category and Phase information in one -- activity
+    # non_audit = df[df['Time Category'].isna()]['Assignment Name']
+    # audit = df[~df['Time Category'].isna()]['Time Category']
+    # df['activity'] = pd.concat((non_audit,audit))
+
+    # # extract year from date, calculate working hours
+    # df['Date'] = pd.to_datetime(df['Date'])
+    # df = df.assign(year_month=df['Date'].dt.strftime('%B, %Y'))
+    # df = df.assign(working_hours=df[['Project Hours', 'Admin Hours']].sum(axis=1))
+
+    # df = df.groupby(['Resource','year_month','time','activity']).sum().drop(columns=['Project Hours', 'Admin Hours',
+    #     'Nonworking Hours', 'Holiday', 'Total (All Entries)',
+    #     'Utilization (All Entries)'])
+
+    # df['monthly_hours'] = df.groupby(level = [0,1]).sum()
+    # df['time_category_hours'] = df.groupby(level = [0,1,2]).sum()[['working_hours']]
+
+    # df['time_category_percentage']  = (100*df['time_category_hours']/df['monthly_hours']).round(1)
+    # df['activity_percentage'] = (100*df['working_hours']/df['time_category_hours']).round(1)
+
+    # df = df.reset_index()
+
+    # df.replace(np.nan,None,inplace=True)
+
+
+    # df = df.astype({'time_category_percentage':'string','activity_percentage':'string'})
+    # df['time_category_percentage']+='%'
+    # df['activity_percentage']+='%'
+
+    # df['time_stat'] = df['time_category_hours'].astype('string') + ' hours (' + df["time_category_percentage"] + ')'
+    # df['activity_hours']  =  df['working_hours'].astype('string') + ' hours (' + df["activity_percentage"] + ')'
+
+    return preprocess_data()
+
+
+df = load_data()
+
+names = list(df['Resource'].unique())
+names.sort()
+
+dates = df[['Date','year_month']].sort_values('Date')['year_month'].unique()
+
+resources = st.multiselect("Resource",names,names)
+dates = st.multiselect("Month",dates,dates[:10])
+
+fig = time_utilisation(df[(df['Resource'].isin(resources)) & (df['year_month'].isin(dates))])
 
 # data_load_state = st.text('Loading data...')
 
@@ -59,4 +68,4 @@ st.set_page_config(layout='centered',page_title='Charged hours breakdwon per sta
 # st.write(df)
 
 # # source = df
-# st.plotly_chart(fig)
+st.plotly_chart(fig)
--- resource_summary/per_staff/utils.py
+++ resource_summary/per_staff/utils.py
@@ -1,6 +1,35 @@
 import plotly.express as px
 import datetime as dt
 import pandas as pd
+import numpy as np
+
+def calc_treemap_data(wb):
+    keys = ['Resource','year_month', 'time','activity']
+    for_treemap = ['time_category_percentage','activity_percentage','time_stat','activity_hours']
+
+    df = wb.groupby(['Resource','year_month','time','activity']).sum().drop(columns=['Project Hours', 'Admin Hours',
+    'Nonworking Hours', 'Holiday', 'Total (All Entries)',
+    'Utilization (All Entries)'])
+
+    df['monthly_hours'] = df.groupby(level = [0,1]).sum()
+    df['time_category_hours'] = df.groupby(level = [0,1,2]).sum()[['working_hours']]
+
+    df['time_category_percentage']  = (100*df['time_category_hours']/df['monthly_hours']).round(1)
+    df['activity_percentage'] = (100*df['working_hours']/df['time_category_hours']).round(1)
+
+    # df = df.reset_index()
+
+    df.replace(np.nan,None,inplace=True)
+
+    df = df.astype({'time_category_percentage':'string','activity_percentage':'string'})
+    df['time_category_percentage']+='%'
+    df['activity_percentage']+='%'
+
+    df['time_stat'] = df['time_category_hours'].astype('string') + ' hours (' + df["time_category_percentage"] + ')'
+    df['activity_hours']  =  df['working_hours'].astype('string') + ' hours (' + df["activity_percentage"] + ')'
+
+    return wb.join(df[for_treemap],on=keys)
+
 
 def preprocess_data(path = '../timesheet_dash_data/RESOURCE SUMMARY.xlsx'):
     wb = pd.read_excel(path,'Sheet1',header=1)
@@ -38,9 +67,23 @@ def preprocess_data(path = '../timesheet_dash_data/RESOURCE SUMMARY.xlsx'):
     idxs = ~wb['Assignment Name'].str.startswith('IOS/').fillna(False)
     prefix_removed[idxs] = wb[idxs]['Assignment Name']
     wb['Assignment Name'] = prefix_removed
+ 
+    wb['Date'] = pd.to_datetime(wb['Date'])
+    # extract year from date
+    wb = wb.assign(year_month=wb['Date'].dt.strftime('%B, %Y'))
+    wb['year'] = wb['Date'].dt.strftime('%B, %Y')
+
+   # merge Time Category and Phase information in one -- activity
+    non_audit = wb[wb['Time Category'].isna()]['Assignment Name']
+    audit = wb[~wb['Time Category'].isna()]['Time Category']
+    wb['activity'] = pd.concat((non_audit,audit))
+
+    # calculate working hours
+    wb['working_hours'] = wb[['Project Hours', 'Admin Hours']].sum(axis=1)
+    wb = wb[wb['time']!='Nonworking']
 
 
-    return wb
+    return calc_treemap_data(wb)
         
 
 def build_treemap(df):
@@ -50,7 +93,7 @@ def build_treemap(df):
     # hovertemplate='label = %{label}<br>%{color:.data}<br>Hours worked = %{value:.2f}'
     hovertemplate = 'label = %{label}<br>Activity = %{customdata[0]}<br>Working hours = %{value}<extra></extra>'
     fig = px.treemap(df,path=[px.Constant('Month'),'year_month','Resource','time','time_stat','activity','activity_hours'],
-    hover_data=['time','activity_hours'],width = 1300,height=800,
+    hover_data=['time','activity_hours'],
     values='working_hours',color = 'time',color_discrete_map=colour_map,maxdepth=5)
 
     fig.update_layout(title='Time Utilization per Month and Auditor',title_x=0.5)
